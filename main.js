c6a46fc1f5b81bf29ae49ed8e996663c9196e9b1
Mohammad Ifteshan <mohdifteshan786@gmail.com>
2025-11-26 20:33:19 +0530

    Upload File

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll",()=>{
-
-    if(window.scroll > 0){
-
-        navbar.classList.add("scroll-nav");
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0){
+        navbar.classList.add('scroll-nav');
     }
     else{
-        navbar.classList.remove("scroll-nav");
+        navbar.classList.remove('scroll-nav');
     }
 })
